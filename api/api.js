982741785951011a48a8975d5023bd61aef90015
ASanti <Darkstars31@gmail.com>
2017-10-22 22:50:31 -0500

    Added Logging with Log4js and readme + ping route

--- api/api.js
+++ api/api.js
@@ -1,6 +1,7 @@
 
 var config  = require('./config');
 var express = require('express');
+var log4js  = require('log4js');
 var app     = express();
 var jwt     = require('jsonwebtoken');
 var bodyparser = require('body-parser');
@@ -10,9 +11,9 @@ app.use(bodyparser.urlencoded({extended: false}));
 app.use(bodyparser.json());
 firebase.initializeApp(config.firebase);
 firebase.auth().signInWithEmailAndPassword(config.firebaseAuth.email,config.firebaseAuth.password).catch(err => { console.log(err)});
-
-
 var dbContext = firebase.database();
+var logger = log4js.getLogger('api');
+log4js.configure(config.log4jsConfig);
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -22,11 +23,17 @@ app.use(function(req, res, next) {
 
 console.log('Listening on localhost:'+ config.express.port);
 
+app.get('/ping', function(req,res,next){
+    res.status(204);
+    res.send();
+});
+
 app.get('/rsvp/:code', function(req, res, next){
     dbContext.ref('/rsvp/families/'+req.params.code).once('value').then(function(snapshot) {
       if(snapshot.exists()){
-        res.send(generateJwt());
+        res.send(generateJwt(snapshot));
       } else {
+        logger.error("Rsvp Code does not exist. Code: " + req.params.code);
         res.send(snapshot.exists()); 
       } 
     });    
@@ -36,14 +43,16 @@ app.patch('/rsvp/:code', validateJwt, function (req, res, next) {
   //validateJwt(req,res);
   dbContext.ref('/rsvp/families/'+req.params.code).once('value').then(function(snapshot) {
     if(snapshot.exists()) {
+      try{
       var family = dbContext.ref('/rsvp/families/');
-      family.child(req.params.code).update(
-        req.body
-      );
-        res.send(true);
-      } else {
-        res.send(false);
+      family.child(req.params.code).update(req.body);
+      res.send(true);
+      } catch(e){
+        logger.error("Failure updating Family: "+req.params.code+" with data "+ JSON.stringify(req.body));
       }
+    } else {
+      res.send(false);
+    }
   });    
 });
 
@@ -51,17 +60,16 @@ function validateJwt(req, res, next) {
   try {
     jwt.verify(req.headers.authorization,config.rsaKeys.public);
   } catch (e) {
-    res.statusCode = 401;
+    res.status(401);
     res.send('Unauthorized');
     return;
   }
   next();
-    
 }
 
-function generateJwt() {
+function generateJwt(payload) {
   let expiration = Math.floor(Date.now() / 1000) + (60 * 60);
-  return jwt.sign({exp: expiration},config.rsaKeys.private, {algorithm: 'RS256'});  
+  return jwt.sign({payload: payload, exp: expiration},config.rsaKeys.private, {algorithm: 'RS256'});  
 }
 
 
---
+++ api/readme.md
@@ -0,0 +1,8 @@
+Wedding RSVP API
+==============
+Simple Express based Node Api Server for the wedding RSVP System
+
+To run:
+```
+node api
+```
